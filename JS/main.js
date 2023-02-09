9055f11664549967f424a428834073ad3d5b8062
Frlendss <121218256+Frlendss@users.noreply.github.com>
2023-02-09 10:39:16 +0500

    first commit

---
+++ JS/main.js
@@ -0,0 +1,117 @@
+// task 3
+
+// let a = 3;
+// let b = 4;
+// let s = a * b;
+// let p = 2 * (a + b);
+// console.log(s, p);
+
+// task 4
+
+// let q = 3.14;
+// let w = 12;
+// let L = q * w;
+// console.log(L);
+
+// task 5
+
+// let e = 4;
+// V = Math.pow(a, 3);
+// S = Math.pow(a, 2) * 6;
+// console.log(V, S);
+
+// task 6
+
+// t = 2;
+// y = 3;
+// u = 4;
+// let Z = t * y * u;
+// let I = 2 * (t * y + y * u + u * t);
+// console.log(Z, I);
+
+// task 7
+
+// let R = 2;
+// const p = 3.14;
+// let L = 2 * 2 * p;
+// let S = p * R ** 2;
+// console.log(L, S);
+
+// task 8
+
+// let a = 3;
+// let b = 4;
+// let E = (a + b) / 2;
+// console.log(E);
+
+// task 9
+
+// let a = 3;
+// let b = 4;
+// let G = Math.sqrt(3 * 4);
+// console.log(G);
+
+// task 10 and 11
+
+// let a = 2;
+// let b = 3;
+// let S = a + b;
+// let D = a * b;
+// let A = a ** 2;
+// let B = b ** 2;
+// let j = Math.sqrt(B);
+// let k = Math.sqrt(A);
+// console.log(S, D, A, B, j, k);
+
+// task 12
+
+// let a = 2;
+// let b = 4;
+// let c = Math.sqrt(a ** 2 + b ** 2);
+// let P = a + b + c;
+// console.log(c, P);
+
+// task 13
+
+// const p = 3.14;
+// let R1 = 6;
+// let R2 = 5;
+// let S1 = p * R1;
+// let S2 = p * R2;
+// let S3 = p * (R1 - R2);
+// console.log(S1, S2, S3);
+
+// task 14
+
+// let L = 4;
+// const p = 3.14;
+// let R = L / (2 * p);
+// let S = p * R ** 2;
+// console.log(R, S);
+
+// task 15
+
+// const p = 3.14;
+// let S = 14;
+// let R = Math.sqrt(S / p);
+// console.log(R);
+
+// task 16
+
+// let x1 = 4;
+// let x2 = 5;
+// let Q = Math.sqrt(Math.pow(x1, 2));
+// let P = Math.sqrt(Math.pow(x2, 2));
+// console.log(Q, P);
+
+// task 25
+
+// let x = 5;
+// let y = 3 * x ** 6 - 6 * x ** 2 - 7;
+// console.log(y);
+
+// task 26
+
+// let x = 5;
+// let y = 4 * (x - 3) ** 6 - 7 * (x - 3) ** 3 + 2;
+// console.log(y);
